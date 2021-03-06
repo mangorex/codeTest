3806bc6306c842ddfece3be7a7990fc0554d20c5
Manuel Antonio <mangorex89@gmail.com>
2021-03-06 16:24:19 +0100

    Reset program

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace codeTest
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
 
