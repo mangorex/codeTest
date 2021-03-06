c830e21fafc9586f976d019f495a1f924873f85a
Manuel Antonio <mangorex89@gmail.com>
2021-03-06 17:27:36 +0100

    Delete innecesary file

--- CurrencyRates.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-
-public class CurrencyRates {
-  public string Disclaimer { get; set; }
-  public string License { get; set; }
-  public int TimeStamp { get; set; }
-  public string Base { get; set; }
-  public Dictionary<string, decimal> Rates { get; set; }
-} 
