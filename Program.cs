a681151d789a56fb3b8cd99cb07cd7775978728a
Manuel Antonio <mangorex89@gmail.com>
2021-03-06 16:28:22 +0100

    Fix main

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace codeTest
 {
     class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
 
