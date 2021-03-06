cb4674ceb3e6da03f0dda8679e4dc69e77a8be77
Manuel Antonio <mangorex89@gmail.com>
2021-03-06 18:12:36 +0100

    Bad functional code with repetitions

--- Program.cs
+++ Program.cs
@@ -12,17 +12,41 @@ namespace codeTest
             var url = "http://127.0.0.1/assetsTree.json";
             var root = _download_serialized_json_data<Root>(url); 
             
-            /*List<Common> Lcommon = root.common;
-            List<Fg> Lfg = root.fg;*/
+            List<Common> Lcommon = root.common;
+            // List<Fg> Lfg = root.fg;
             List<P6> Lp6 = root.p6;
             var myCount = 0;
 
             Console.WriteLine(root.p6.Count.ToString());
 
+            foreach(Common itemC in Lcommon){
+                List<PtfTerminal> Lptf_Terminal = itemC.ptf_Terminal;
+                foreach(PtfTerminal itemPT in Lptf_Terminal)
+                {
+                    /*List<object> Lskins = itemPT.skins;
+                    List<Help> Lhelp = itemPT.help;*/
+
+                    if (itemPT.languages != null){
+                        List<Language> Llanguages = itemPT.languages;
+
+                        foreach(Language itemL in Llanguages){
+                            bool required = itemL.required;
+
+                            if (required){
+                                string name = itemL.name;
+                                string md5 = itemL.md5;
+                                Console.WriteLine(required.ToString() + " " + name + " " + md5);
+                            }
+                        }     
+                    }
+                    
+                }
+            }
+
             foreach(P6 itemP in Lp6)
             {
-                List<Common> Lcommon2 = itemP.common;
-                List<Lang> Llang= itemP.lang;
+                /*List<Common> Lcommon2 = itemP.common;
+                List<Lang> Llang= itemP.lang;*/
                 bool required = itemP.required;
 
                 if (required){
