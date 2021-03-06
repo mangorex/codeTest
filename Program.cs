5c3e861e4eaa0cc80a7a568a7e301266b7fffb83
Manuel Antonio <mangorex89@gmail.com>
2021-03-06 21:53:05 +0100

    More clarity code with 2 functions. 2 more tests

--- Program.cs
+++ Program.cs
@@ -9,66 +9,114 @@ namespace codeTest
     {
         static void Main(string[] args)
         {
-            Leaf leaf; 
-
             var url = "http://127.0.0.1/assetsTree.json";
-            var root = downloadSerializedJson<Root>(url); 
-            
+            var root = downloadSerializedJson<Root>(url);
+
             List<Common> Lcommon = root.common;
             // List<Fg> Lfg = root.fg;
             List<P6> Lp6 = root.p6;
-            var myCount = 0;
 
-            Console.WriteLine(root.p6.Count.ToString());
+            loopCommonList(Lcommon);
+        }
 
-            foreach(Common itemC in Lcommon){
-                List<PtfTerminal> Lptf_Terminal = itemC.ptf_Terminal;
-                foreach(PtfTerminal itemPT in Lptf_Terminal)
+        public static bool loopCommonList(List<Common> Lcommon)
+        {
+            Leaf leaf;
+
+            try
+            {
+                foreach (Common itemC in Lcommon)
                 {
-                    List<Help> Lhelp = itemPT.help;
+                    List<PtfTerminal> Lptf_Terminal = itemC.ptf_Terminal;
+                    foreach (PtfTerminal itemPT in Lptf_Terminal)
+                    {
+                        if (itemPT.help != null)
+                        {
+                            List<Help> Lhelp = itemPT.help;
 
-                    if (itemPT.languages != null){
-                        List<Language> Llanguages = itemPT.languages;
+                            foreach (Help itemHP in Lhelp)
+                            {
+                                if (itemHP.en_GB != null)
+                                {
+                                    List<EnGB> Len_GB = itemHP.en_GB;
+                                    foreach (EnGB itemGB in Len_GB)
+                                    {
+                                        leaf = new Leaf(itemGB.md5, itemGB.name, itemGB.required);
+                                        checkLeaf(leaf);
+                                    }
+                                }
 
-                        foreach(Language itemL in Llanguages){
-                            leaf = new Leaf(itemL.md5, itemL.name, itemL.required);
-                            checkLeaf(leaf);
-                        }     
+                                leaf = new Leaf(itemHP.md5, itemHP.name, itemHP.required);
+                                checkLeaf(leaf);
+                            }
+                        }
+
+                        if (itemPT.languages != null)
+                        {
+                            List<Language> Llanguages = itemPT.languages;
+
+                            foreach (Language itemL in Llanguages)
+                            {
+                                leaf = new Leaf(itemL.md5, itemL.name, itemL.required);
+                                checkLeaf(leaf);
+                            }
+                        }
                     }
                 }
             }
-
-            foreach(P6 itemP in Lp6)
+            catch (Exception)
             {
-                /*List<Common> Lcommon2 = itemP.common;
-                List<Lang> Llang= itemP.lang;*/
-                leaf = new Leaf(itemP.md5, itemP.name, itemP.required);
-                checkLeaf(leaf);
-                myCount++;
+                return false;
             }
+            return true;
+        }
+
+        public static bool loopP6(List<P6> Lp6)
+        {
+            Leaf leaf;
+
+            try
+            {
+                foreach (P6 itemP in Lp6)
+                {
+                    /*List<Common> Lcommon2 = itemP.common;
+                    List<Lang> Llang= itemP.lang;*/
+                    leaf = new Leaf(itemP.md5, itemP.name, itemP.required);
+                    checkLeaf(leaf);
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            return true;
         }
 
         static bool checkLeaf(Leaf leaf)
         {
-            if (leaf.required){
-                Console.WriteLine(leaf.required.ToString() + " " + leaf.name + " " + leaf.md5);
+            if (leaf.required)
+            {
+                Console.WriteLine(leaf.md5 + " " + leaf.name + " " + leaf.required.ToString());
                 return true;
             }
             return false;
         }
 
-         public static T downloadSerializedJson<T>(string url) where T : new() {
-            using (var w = new WebClient()) {
+        public static T downloadSerializedJson<T>(string url) where T : new()
+        {
+            using (var w = new WebClient())
+            {
                 var json_data = string.Empty;
                 // attempt to download JSON data as a string
-                try {
+                try
+                {
                     json_data = w.DownloadString(url);
                 }
-                catch (Exception) {}
+                catch (Exception) { }
                 // if string with JSON data is not empty, deserialize it to class and return its instance 
                 return !string.IsNullOrEmpty(json_data) ? JsonConvert.DeserializeObject<T>(json_data) : new T();
             }
         }
     }
-    
+
 }
--- ProgramNunit.cs
+++ ProgramNunit.cs
@@ -10,23 +10,40 @@ namespace UnitTesting.GettingStarted.Tests
     [TestFixture]
     public class ProgramNunit
     {
+        static string url = "http://127.0.0.1/assetsTree.json";
+        static Root root = Program.downloadSerializedJson<Root>(url); 
 
         [Test]
         public void TestDownloadSerializedJsonP6BasicCount()
         {
-            var url = "http://127.0.0.1/assetsTree.json";
-            var root = Program.downloadSerializedJson<Root>(url); 
             Assert.AreEqual(3, root.p6.Count);
         }
-
 
         [Test]
         public void TestDownloadSerializedJsonP6Basic()
         {
-            var url = "http://127.0.0.1/assetsTree.json";
-            var root = Program.downloadSerializedJson<Root>(url); 
             Console.WriteLine(root.p6.Count.ToString());
             Assert.AreEqual(true, root.p6[2].required);
         }
+
+        [Test]
+        public void TestLoopCommonListBasic()
+        {
+            List<Common> Lcommon = root.common;
+            bool ok = Program.loopCommonList(Lcommon);
+            Console.WriteLine(Lcommon.Count.ToString());
+            Assert.AreEqual(true, ok);
+        }
+
+        
+        [Test]
+        public void TestLoopP6Basic()
+        {
+            List<P6> Lp6 = root.p6;
+            bool ok = Program.loopP6(Lp6);
+            Console.WriteLine(Lp6.Count.ToString());
+            Assert.AreEqual(true, ok);
+        }
+        
     }
 }
